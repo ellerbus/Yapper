43c18222dc71833e1a7b45aee12fd4977d9934ce
Stu Ellerbusch <stu.ellerbusch@gmail.com>
2014-08-15 13:27:28 -0400

    Added CacheManager

---
+++ Yapper.Tests/Cache/CacheManagerTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yapper.Cache;
+
+namespace Yapper.Tests.Cache
+{
+    [TestClass]
+    public class CacheManagerTests
+    {
+        [TestMethod]
+        public void CacheManager_Should_AddAndExpire()
+        {
+            CacheManager sut = new CacheManager();
+
+            var user = new { ID =1,Name="Me" };
+
+            CacheKey key = new CacheKey("GETUSER-"+user.ID, new[] { "Users", "User-" + user.ID });
+
+            CachePolicy cp = new CachePolicy { AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(60) };
+
+            sut.Add(key, user, cp);
+
+            Assert.IsNotNull(sut.Get(key));
+
+            sut.Expire("Users");
+
+            Assert.IsNull(sut.Get(key));
+        }
+
+        [TestMethod]
+        public void CacheManager_Should_AddAndRemove()
+        {
+            CacheManager sut = new CacheManager();
+
+            var user = new { ID = 2, Name = "Me" };
+
+            CacheKey key = new CacheKey("GETUSER-" + user.ID, new[] { "Users", "User-" + user.ID });
+
+            CachePolicy cp = new CachePolicy { AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(60) };
+
+            sut.Add(key, user, cp);
+
+            Assert.IsNotNull(sut.Get(key));
+
+            object userRemoved = sut.Remove(key);
+
+            Assert.AreEqual(user, userRemoved);
+        }
+    }
+}
---
+++ Yapper/Cache/CacheKey.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnsureThat;
+
+namespace Yapper.Cache
+{
+    /// <summary>
+    /// A class representing a unique key for cache items.
+    /// </summary>
+    public class CacheKey
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CacheKey"/> class.
+        /// </summary>
+        /// <param name="key">The key for a cache item.</param>
+        public CacheKey(string key)
+            : this(key, Enumerable.Empty<string>())
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CacheKey"/> class.
+        /// </summary>
+        /// <param name="key">The key for a cache item.</param>
+        /// <param name="tags">The tags for the cache item.</param>
+        public CacheKey(string key, IEnumerable<string> tags)
+        {
+            Ensure.That(key, "key").IsNotNullOrWhiteSpace();
+
+            Key = key;
+
+            if (tags != null)
+            {
+                Tags = new HashSet<string>(tags);
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the key for a cached item.
+        /// </summary>
+        public string Key { get; private set; }
+
+        /// <summary>
+        /// Gets the tags for a cached item.
+        /// </summary>
+        public HashSet<string> Tags { get; private set; }
+
+        #endregion
+    }
+}
