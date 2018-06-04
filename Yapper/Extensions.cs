04591aae54ba764a5ed1e28feff7772641425f3d
Stu <stu.ellerbusch@gmail.com>
2018-06-04 09:05:31 -0400

    Bug Fix

---
+++ Yapper/Extensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+
+namespace Yapper
+{
+    internal static class Extensions
+    {
+        public static string GetTableName(this Type type)
+        {
+            string table = type.Name;
+
+            TableAttribute ta = type.GetCustomAttribute<TableAttribute>();
+
+            if (ta != null)
+            {
+                table = ta.Name;
+            }
+
+            return table;
+        }
+
+        public static string GetColumnName(this PropertyInfo p)
+        {
+            string column = p.Name;
+
+            ColumnAttribute ca = p.GetCustomAttribute<ColumnAttribute>();
+
+            if (ca != null)
+            {
+                column = ca.Name;
+            }
+
+            return column;
+        }
+
+        public static bool IsColumn(this PropertyInfo p)
+        {
+            return Attribute.GetCustomAttribute(p, typeof(ColumnAttribute), true) != null;
+        }
+
+        public static bool IsPrimaryKey(this PropertyInfo p)
+        {
+            return Attribute.GetCustomAttribute(p, typeof(KeyAttribute), true) != null;
+        }
+
+        public static bool IsDatabaseGenerated(this PropertyInfo p)
+        {
+            return Attribute.GetCustomAttribute(p, typeof(DatabaseGeneratedAttribute), true) != null;
+        }
+
+        public static bool IsIdentity(this PropertyInfo p)
+        {
+            Attribute attr = Attribute.GetCustomAttribute(p, typeof(DatabaseGeneratedAttribute), true);
+
+            DatabaseGeneratedAttribute dga = attr as DatabaseGeneratedAttribute;
+
+            if (dga != null)
+            {
+                return dga.DatabaseGeneratedOption == DatabaseGeneratedOption.Identity;
+            }
+
+            return false;
+        }
+
+        public static IList<PropertyInfo> GetColumnProperties(this Type type)
+        {
+            IList<PropertyInfo> properties = type
+                .GetProperties()
+                .Where(x => x.IsColumn())
+                .ToList();
+
+            return properties;
+        }
+    }
+}
--- Yapper/StatementBuilder.cs
+++ Yapper/StatementBuilder.cs
@@ -9,76 +9,6 @@ using Augment;
 
 namespace Yapper
 {
-    internal static class Extensions
-    {
-        public static string GetTableName(this Type type)
-        {
-            string table = type.Name;
-
-            TableAttribute ta = type.GetCustomAttribute<TableAttribute>();
-
-            if (ta != null)
-            {
-                table = ta.Name;
-            }
-
-            return table;
-        }
-
-        public static string GetColumnName(this PropertyInfo p)
-        {
-            string column = p.Name;
-
-            ColumnAttribute ca = p.GetCustomAttribute<ColumnAttribute>();
-
-            if (ca != null)
-            {
-                column = ca.Name;
-            }
-
-            return column;
-        }
-
-        public static bool IsColumn(this PropertyInfo p)
-        {
-            return Attribute.GetCustomAttribute(p, typeof(ColumnAttribute), true) != null;
-        }
-
-        public static bool IsPrimaryKey(this PropertyInfo p)
-        {
-            return Attribute.GetCustomAttribute(p, typeof(KeyAttribute), true) != null;
-        }
-
-        public static bool IsDatabaseGenerated(this PropertyInfo p)
-        {
-            return Attribute.GetCustomAttribute(p, typeof(DatabaseGeneratedAttribute), true) != null;
-        }
-
-        public static bool IsIdentity(this PropertyInfo p)
-        {
-            Attribute attr = Attribute.GetCustomAttribute(p, typeof(DatabaseGeneratedAttribute), true);
-
-            DatabaseGeneratedAttribute dga = attr as DatabaseGeneratedAttribute;
-
-            if (dga != null)
-            {
-                return dga.DatabaseGeneratedOption == DatabaseGeneratedOption.Identity;
-            }
-
-            return false;
-        }
-
-        public static IList<PropertyInfo> GetColumnProperties(this Type type)
-        {
-            IList<PropertyInfo> properties = type
-                .GetProperties()
-                .Where(x => x.IsColumn())
-                .ToList();
-
-            return properties;
-        }
-    }
-
     /// <summary>
     ///
     /// </summary>
@@ -355,7 +285,6 @@ namespace Yapper
                 IList<PropertyInfo> properties = type.GetColumnProperties().ToList();
 
                 string cols = properties
-                    .Where(x => !x.IsDatabaseGenerated())
                     .Select(x => $"{x.GetColumnName()} {x.Name}")
                     .Join($",{NL}{S8}");
 
@@ -396,7 +325,6 @@ namespace Yapper
                 IList<PropertyInfo> properties = type.GetColumnProperties().ToList();
 
                 string cols = properties
-                    .Where(x => !x.IsDatabaseGenerated())
                     .Select(x => $"{x.GetColumnName()} {x.Name}")
                     .Join($",{NL}{S8}");
 
